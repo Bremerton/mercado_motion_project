a96c95eae0a0e67855d3cd1c3ba2c246ba8fa79e
Bremerton <72746036+Bremerton@users.noreply.github.com>
2021-02-04 10:39:49 -0800

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print(input.acceleration(Dimension.Z))
+print(input.rotation(Rotation.ROLL))
 while True:
-    if input.acceleration(Dimension.Z) > 0 or input.acceleration(Dimension.Z)  < 0 and input.light_level() < 8:
+    if input.rotation(Rotation.ROLL) < 0:
         light.set_all(light.rgb(255, 255, 255))
+    elif input.light_level() < 10:
+        light.clear()
+
+        input.acceleration(Dimension.Z)
+        input.rotation(Rotation.PITCH)
--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-console.log(input.acceleration(Dimension.Z))
+console.log(input.rotation(Rotation.Roll))
 while (true) {
-    if (input.acceleration(Dimension.Z) > 0 || input.acceleration(Dimension.Z) < 0 && input.lightLevel() < 8) {
+    if (input.rotation(Rotation.Roll) < 0) {
         light.setAll(light.rgb(255, 255, 255))
+    } else if (input.lightLevel() < 10) {
+        light.clear()
+        input.acceleration(Dimension.Z)
+        input.rotation(Rotation.Pitch)
     }
     
 }
